5304eae9a2f99caefeccb3052fcdfb7ea2a096dc
BatuhanOguzz <batuhan.oguz06@outlook.com>
2025-07-12 16:32:47 +0300

    Add Camera Rotation

--- Assets/Synty/AnimationBaseLocomotion/Samples/Scripts/SampleCameraController.cs
+++ Assets/Synty/AnimationBaseLocomotion/Samples/Scripts/SampleCameraController.cs
@@ -41,13 +41,23 @@ namespace Synty.AnimationBaseLocomotion.Samples
         [SerializeField]
         private float _cameraHorizontalOffset;
         [SerializeField]
-        private float _cameraTiltOffset;
+        private float _cameraTiltOffset; // Eskisi gibi dursun, uyumluluk için
         [SerializeField]
         private Vector2 _cameraTiltBounds = new Vector2(-10f, 45f);
         [SerializeField]
         private float _positionalCameraLag = 1f;
         [SerializeField]
         private float _rotationalCameraLag = 1f;
+
+        // Yeni Offsetler
+        [Header("Camera Rotation Offsets")]
+        [SerializeField]
+        private float _cameraPitchOffset = 0f;   // X ekseni (aþaðý/yukarý)
+        [SerializeField]
+        private float _cameraYawOffset = 0f;     // Y ekseni (saða/sola)
+        [SerializeField]
+        private float _cameraRollOffset = 0f;    // Z ekseni (eðilme)
+
         private float _cameraInversion;
 
         private InputReader _inputReader;
@@ -57,7 +67,6 @@ namespace Synty.AnimationBaseLocomotion.Samples
         private Vector3 _lastPosition;
 
         private float _newAngleX;
-
         private float _newAngleY;
         private Vector3 _newPosition;
         private float _rotationX;
@@ -65,7 +74,6 @@ namespace Synty.AnimationBaseLocomotion.Samples
 
         private Transform _syntyCamera;
 
-        /// <inheritdoc cref="Start" />
         private void Start()
         {
             _syntyCamera = gameObject.transform.GetChild(0);
@@ -88,17 +96,15 @@ namespace Synty.AnimationBaseLocomotion.Samples
             _lastPosition = transform.position;
 
             _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, _cameraDistance * -1);
-            _syntyCamera.localEulerAngles = new Vector3(_cameraTiltOffset, 0f, 0f);
+            SetCameraRotation();
         }
 
-        /// <inheritdoc cref="Update" />
         private void Update()
         {
             float positionalFollowSpeed = 1 / (_positionalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
             float rotationalFollowSpeed = 1 / (_rotationalCameraLag / _LAG_DELTA_TIME_ADJUSTMENT);
 
             _rotationX = _inputReader._mouseDelta.y * _cameraInversion * _mouseSensitivity;
-
             _rotationY = _inputReader._mouseDelta.x * _mouseSensitivity;
 
             _newAngleX += _rotationX;
@@ -125,87 +131,66 @@ namespace Synty.AnimationBaseLocomotion.Samples
             transform.eulerAngles = new Vector3(_newAngleX, _newAngleY, 0);
 
             _syntyCamera.localPosition = new Vector3(_cameraHorizontalOffset, _cameraHeightOffset, _cameraDistance * -1);
-            _syntyCamera.localEulerAngles = new Vector3(_cameraTiltOffset, 0f, 0f);
+
+            SetCameraRotation();
 
             _lastPosition = _newPosition;
             _lastAngleX = _newAngleX;
             _lastAngleY = _newAngleY;
         }
 
-        /// <summary>
-        ///     Locks the camera to aim at a specified target.
-        /// </summary>
-        /// <param name="enable">Whether lock on is enabled or not.</param>
-        /// <param name="newLockOnTarget">The target to lock on to.</param>
+        // Offsetlerle kameranýn lokal rotasyonunu ayarla
+        private void SetCameraRotation()
+        {
+            // _cameraTiltOffset legacy için dursun, istersen silebilirsin
+            _syntyCamera.localEulerAngles = new Vector3(
+                _cameraTiltOffset + _cameraPitchOffset,  // X ekseni (tilt + pitch offset)
+                _cameraYawOffset,                        // Y ekseni (yaw offset)
+                _cameraRollOffset                        // Z ekseni (roll offset)
+            );
+        }
+
+        // Diðer metodlar ayný kalýyor...
+
         public void LockOn(bool enable, Transform newLockOnTarget)
         {
             _isLockedOn = enable;
-
             if (newLockOnTarget != null)
             {
                 _lockOnTarget = newLockOnTarget;
             }
         }
 
-        /// <summary>
-        /// Gets the position of the camera.
-        /// </summary>
-        /// <returns>The position of the camera.</returns>
         public Vector3 GetCameraPosition()
         {
             return _mainCamera.transform.position;
         }
 
-        /// <summary>
-        /// Gets the forward vector of the camera.
-        /// </summary>
-        /// <returns>The forward vector of the camera.</returns>
         public Vector3 GetCameraForward()
         {
             return _mainCamera.transform.forward;
         }
 
-        /// <summary>
-        /// Gets the forward vector of the camera with the Y value zeroed.
-        /// </summary>
-        /// <returns>The forward vector of the camera with the Y value zeroed.</returns>
         public Vector3 GetCameraForwardZeroedY()
         {
             return new Vector3(_mainCamera.transform.forward.x, 0, _mainCamera.transform.forward.z);
         }
 
-        /// <summary>
-        /// Gets the normalised forward vector of the camera with the Y value zeroed.
-        /// </summary>
-        /// <returns>The normalised forward vector of the camera with the Y value zeroed.</returns>
         public Vector3 GetCameraForwardZeroedYNormalised()
         {
             return GetCameraForwardZeroedY().normalized;
         }
 
-
-        /// <summary>
-        /// Gets the right vector of the camera with the Y value zeroed.
-        /// </summary>
-        /// <returns>The right vector of the camera with the Y value zeroed.</returns>
         public Vector3 GetCameraRightZeroedY()
         {
             return new Vector3(_mainCamera.transform.right.x, 0, _mainCamera.transform.right.z);
         }
 
-        /// <summary>
-        /// Gets the normalised right vector of the camera with the Y value zeroed.
-        /// </summary>
-        /// <returns>The normalised right vector of the camera with the Y value zeroed.</returns>
         public Vector3 GetCameraRightZeroedYNormalised()
         {
             return GetCameraRightZeroedY().normalized;
         }
 
-        /// <summary>
-        /// Gets the X value of the camera tilt.
-        /// </summary>
-        /// <returns>The X value of the camera tilt.</returns>
         public float GetCameraTiltX()
         {
             return _mainCamera.transform.eulerAngles.x;
